9fa1e15dd1f624828654fc78e1f72ba840799cee
Rockwell McGellin <rockwell.m@gmail.com>
2015-02-24 07:59:24 +0000

    stupid line break

--- titp_write.py
+++ titp_write.py
@@ -64,8 +64,7 @@ def processform(): #This collects the data from the form (add.html)
 def generate(): #Builds the timetable for all users.
 	s = str()
 	s +=  '<p>Currently tracking ' + str(len(tt['users'])) + ' people\'s availability between classes.</p>\n\n'
-	s +=  '<div style="padding:1em"><table style="width:100%" 
-border="1">\n<tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></tr>\n'
+	s +=  '<div style="padding:1em"><table style="width:100%" border="1">\n<tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></tr>\n'
 	
 	for h in times:
 		s +=  "<tr><td>" + str(h) + '</td>'
